7c33fcb632ca66e010383abf3c820729a70be366
jemin-balar <balarjemin701@gmail.com>
2023-12-08 11:36:13 +0530

    Creating Pages for Codeswear

---
+++ pages/product/[slug].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return <p>Product: {slug}</p>;
+};
+export default Slug;
